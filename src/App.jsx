5362b888852d04e2f27f8e16d2c7ffac97935919
Your Name <mostafaahmed1101997@gmail.com>
2024-10-28 11:12:21 +0300

    upload spotify

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useContext } from 'react'
+import './App.css'
+import Display from './component/Display'
+import Player from './component/Player'
+import Sidebar from './component/Sidebar'
+import { PlayerContext } from './context/PlayerContext'
+
+function App() {
+ 
+  const {audioRef,track}=useContext(PlayerContext )
+
+  return (
+    <>
+    <div className='bg-black h-screen'>
+      <div className='h-[90%] flex'>
+      <Sidebar/>
+      <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+    </div>
+    </>
+  )
+}
+
+export default App
